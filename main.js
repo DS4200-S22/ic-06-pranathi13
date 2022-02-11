3d353f3117b3f4330ae6ff812c069935b7da9749
unknown <alluri.p@northeastern.edu>
2022-02-10 22:49:01 -0500

    Created style.css and added js to index.html

--- main.js
+++ main.js
@@ -9,6 +9,11 @@ Modified: 12/06/21
 //Starter code for Button 1 click
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
+
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    let newText = Math.random();  
+    // we need to select the button-div (using its id) 
+    let buttonDiv = document.getElementById("random_num"); 
+    // then we buttonDiv's content
+    buttonDiv.innerHTML = newText; 
 }
